7d47aaa4c54219571a4a5ccd2dd4648082557d60
Matt <solomatias321@gmail.com>
2025-06-07 17:17:00 +0200

    fix: run migration to postgres sql in neon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,3 +3,4 @@ import  express from "express";
 const app = express();
 
 //postgres + prisma => ORM
+//postgresql://neondb_owner:npg_nBcjO85YskJp@ep-morning-smoke-a5y97g8c-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require
